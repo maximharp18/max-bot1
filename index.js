0398da7ebb5b342f009935d680947baa0ae8004f
maximharp18 <maximharp18@gmail.com>
2026-01-22 12:42:38 +0400

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
 app.post('/webhook', (req, res) => {
-  console.log('WEBHOOK:', req.body);
+  console.log('=== WEBHOOK ===');
+  console.log(req.headers);
+  console.log(JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Server started on port', PORT);
+  console.log(`Server started on port ${PORT}`);
 });
